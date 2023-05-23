45b006760fb9ffd831e65db86e8a2a0ad6cb8359
daw223-03 <122364966+daw223-03@users.noreply.github.com>
2023-05-23 16:44:56 +0200

    Update DecoappContext.cs

--- DecoApp4/Models/DecoappContext.cs
+++ DecoApp4/Models/DecoappContext.cs
@@ -50,9 +50,9 @@ public partial class DecoappContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server=DESKTOP-3IT3POC\\DESARROLLO; database=DECOAPP; Trusted_Connection=True; Trust Server Certificate=True;");
+        => optionsBuilder.UseSqlServer("server=DESKTOP-3IT3POC\\DESARROLLO; database=DECOAPP; Trusted_Connection=True; Trust Server Certificate=True;");*/
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
